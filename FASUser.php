e4c9f33cee2c1d37d59b5560c15d63df60ff8691
Ricky Elrod <ricky@elrod.me>
2012-11-08 17:50:09 -0500

    Add a bunch of setter methods for FASUser fields.

--- FASUser.php
+++ FASUser.php
@@ -13,5 +13,54 @@ class FASUser {
   protected $groups = array();
   protected $ssh_key;
   protected $gpg_key;
-  protected $password;
+
+  /**
+   * Set the username of the representation of the FAS user.
+   *
+   * @task setter
+   */
+  public function setUsername($username) {
+    $this->username = $username;
+    return $this;
+  }
+
+  /**
+   * Set the real name of the representation of the FAS user.
+   *
+   * @task setter
+   */
+  public function setRealName($real_name) {
+    $this->real_name = $real_name;
+    return $this;
+  }
+
+  /**
+   * Appends a group to the `groups` array of arrays.
+   *
+   * @task setter
+   */
+  public function appendGroup($group) {
+    $this->groups[] = $group;
+    return $this;
+  }
+
+  /**
+   * Set the ssh key of the representation of the FAS user.
+   *
+   * @task setter
+   */
+  public function setSshKey($ssh_key) {
+    $this->ssh_key = $ssh_key;
+    return $this;
+  }
+
+  /**
+   * Set the gpg key id of the representation of the FAS user.
+   *
+   * @task setter
+   */
+  public function setGpgKey($gpg_key) {
+    $this->gpg_key = $gpg_key;
+    return $this;
+  }
 }
