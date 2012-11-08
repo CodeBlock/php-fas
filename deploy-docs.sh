d9abe2d7b2ddc6d56ae623445e411fc6d867f78f
Ricky Elrod <ricky@elrod.me>
2012-11-08 15:50:56 -0500

    update docs deploy script a bit.

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 uid=$(mktemp -u)
+uid2=$(mktemp -u)
 git clone git@github.com:CodeBlock/php-fas $uid
 pushd $uid
   diviner .
   rm -rf .divinercache
   git checkout gh-pages
-  mv docs/* .
-  rmdir docs
+  mv docs/ $uid2
+  git rm -rf $uid/*
+  mv $uid2/* .  
   git add .
   git commit -m "Deploying documentation at `date`"
   git push origin gh-pages
 popd
-rm -rf $uid
+rm -rf $uid $uid2
